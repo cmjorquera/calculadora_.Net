f9fe8aee466c7277b7de69683b2f27000c38eb6f
Cristian Jorquera <cm.jorquerag@gmail.com>
2021-02-27 14:25:41 -0300

    calculadora en .net

---
+++ calculadora/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables and then initialize to zero.
+            float num1 = 0; float num2 = 0;
+
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Calculadora de consola en C#\r");
+            Console.WriteLine("Autor:Cristian Jorquera ");
+            Console.WriteLine("------------------------\n");
+
+            // Ask the user to type the first number.
+            Console.WriteLine("Escriba un numero y luego presine Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to type the second number.
+            Console.WriteLine("Escriba otro numero y luego presione Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Eliga una opcion de la sigueinte Lista:");
+            Console.WriteLine("\ta - Sumar");
+            Console.WriteLine("\ts - Restar");
+            Console.WriteLine("\tm - Multilicar");
+            Console.WriteLine("\td - Dividir");
+            Console.Write("Tu opcion es? ");
+
+            // Use a switch statement to do the math.
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"tu resultado: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"tu resultado: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"tu resultado: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"tu resultado: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            // Wait for the user to respond before closing.
+            Console.Write("Presiona cualquier tecla para cerrar la aplicaion de la consola");
+            Console.ReadKey();
+        }
+    }
+}
